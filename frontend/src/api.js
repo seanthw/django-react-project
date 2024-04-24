51c9041185882686fadb3aaba1ef60375ac89389
seanthw <sean.thawe@outlook.com>
2024-04-24 04:15:03 +0200

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
+const apiUrl = "/choreo-apis/djangoreactproject/backend/rest-api-be2/v1.0"
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 });
 
 api.interceptors.request.use(
